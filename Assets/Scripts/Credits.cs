77b2dcefcc7984a216d207b73d9ea2aacce47d7e
Andrew Albizati <aalbizati@sandiego.edu>
2022-10-26 18:18:49 -0700

    Made final changes to credits

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void TitleScreenClicked()
     {
         SceneManager.LoadScene("Menu");
